142d2b6aad558665623f2f8d81bbe58badaf72db
Manish Kumar <sihagmanish36@gmail.com>
2022-10-30 12:31:12 -0500

    Updated the parser to make the metadata as the central point of information

--- src/cord19-parser.py
+++ src/cord19-parser.py
@@ -1,16 +1,15 @@
 import csv
-import pandas as pd
-import numpy as np
 import json
-from tqdm import tqdm
-import click
-from glob import glob
 import os
+
+import click
+import pandas as pd
+from tqdm import tqdm
 
 pj = os.path.join
 
 
-def load_json(path):
+def load_json(path: str):
     """
     Load a JSON file
     :param path: Path to the JSON file
@@ -36,10 +35,14 @@ def get_authors(json_file):
 
 
 def get_affiliations():
-    return
+    """
+    TODO: Implement later if needed
+    :return:
+    """
+    pass
 
 
-def get_abstract(json_file, pmc):
+def get_abstract(json_file, pmc: bool):
     """
     Get the abstract of the paper
     :param json_file: JSON file object
@@ -70,35 +73,85 @@ def get_body_text(json_file):
     return body_text
 
 
-def parse_jsons(json_paths, paper_df, ignore_missing_titles, pmc):
+def parse_json(json_path: str, pmc: bool):
     """
     Parse the JSONs and extract relevant data from them
-    :param json_paths: A list of file paths
-    :param paper_df: A CSV object to write the data to
-    :param ignore_missing_titles: Whether to ignore the papers with missing titles
+    :param json_path: File path to the JSON file
     :param pmc: PMC papers have slightly different structure so needs additional processing
-    :return:
+    :return: A list with fetched data
     """
-    for p in tqdm(json_paths[0:5000]):
-        paper = load_json(p)
-        paper_id = paper['paper_id']
-        title = paper["metadata"]["title"]
+    paper = load_json(json_path)
+    paper_id = paper['paper_id']
+    title = paper["metadata"]["title"]
+    authors = get_authors(paper)
+    abstract = get_abstract(paper, pmc)
+    body_text = get_body_text(paper)
 
-        if (not title) and ignore_missing_titles:
-            continue
+    return [paper_id, title, authors, abstract, body_text]
 
-        authors = get_authors(paper)
-        abstract = get_abstract(paper, pmc)
-        body_text = get_body_text(paper)
 
-        paper_df.writerow([paper_id, title, authors, abstract, body_text])
+def handle_pmc_json(metadata: pd.DataFrame, index: int, pmc_path: str, path: str):
+    """
+    If PMC json is available, parse it and try to get as many details as possible
+    :param metadata: The metadata CSV file
+    :param index: Index of the row to process
+    :param pmc_path: Path to the PMC json file
+    :param path: Top level data directory
+    :return: A list of features extracted from the JSONs
+    """
+    pmc_data = parse_json(pmc_path, pmc=True)
+    if len(pmc_data) > 0:
+
+        # Handle the missing title cases
+        if not pmc_data[1]:
+            if not pd.isna(metadata.title[index]):
+                pmc_data[1] = metadata.title[index]
+
+        # Handle the missing abstract cases
+        if not pmc_data[3] and not pd.isna(metadata.abstract[index]):
+            pmc_data[3] = metadata.abstract[index]
+        else:
+            pdf_paths = metadata.pdf_json_files[index]
+            if not pd.isna(pdf_paths):
+                pdf_paths = [x.strip() for x in pdf_paths.split(";")]
+                for p in pdf_paths:
+                    p = pj(path, p)
+                    pdf_data = parse_json(p, pmc=False)
+                    if pdf_data[3]:
+                        pmc_data[3] = pdf_data[3]
+                        break
+
+    return pmc_data
+
+
+def handle_pdf_json(metadata: pd.DataFrame, index: int, pdf_path: str):
+    """
+    If pdf json is available, parse it and try to get as many details as possible
+    :param metadata: The metadata CSV file
+    :param index: Index of the row to process
+    :param pdf_path: Path to the pdf json file
+    :return: A list of features extracted from the JSONs
+    """
+    pdf_data = parse_json(pdf_path, pmc=False)
+    if len(pdf_data) > 0:
+
+        # Handle the missing title cases
+        if not pdf_data[1]:
+            if not pd.isna(metadata.title[index]):
+                pdf_data[1] = metadata.title[index]
+
+        # Handle the missing abstract cases
+        if not pdf_data[3] and not pd.isna(metadata.abstract[index]):
+            pdf_data[3] = metadata.abstract[index]
+
+    return pdf_data
 
 
 @click.command()
 @click.argument('path', nargs=1, type=click.Path(exists=True))
-@click.option('-i', '--ignore-missing-titles', default=True, required=False, type=bool,
-              help="Whether to ignore the papers with missing titles")
-def main(path, ignore_missing_titles):
+# @click.option('-i', '--ignore-missing-titles', default=True, required=False, type=bool,
+#               help="Whether to ignore the papers with missing titles")
+def main(path):
     """
     Expected directory structure
     path
@@ -108,24 +161,49 @@ def main(path, ignore_missing_titles):
             |___ pdf_json
             |___ pmc_json
 
-    :param ignore_missing_titles:
     :param path: Path to the folder where the file is unzipped
     :return:
     """
+
     # Initiliaze a CSV write object. Using pandas dataframe is note a good idea because the dataset size is huge and
     # may not fit in the machine memory
     paper_df = csv.writer(open(path + "parsed_papers.csv", 'w'))
-    paper_df.writerow(["paper_id", "title", "authors", "abstract", "body_text"])
-
-    print("Fetching list of file paths")
-    pdf_paths = glob(pj(path, "document_parses/pdf_json/*.json"))
-    pmc_paths = glob(pj(path, "document_parses/pmc_json/*.json"))
+    paper_df.writerow(["paper_id", "title", "authors", "abstract", "body_text", "publish_time", "journal"])
 
     print("Loading metadata csv file")
-    # metadata = pd.read_csv("metadata.csv")
+    metadata = pd.read_csv(pj(path, "metadata.csv"))
 
-    parse_jsons(pdf_paths, paper_df, ignore_missing_titles, pmc=False)
-    parse_jsons(pmc_paths, paper_df, ignore_missing_titles, pmc=True)
+    paper_count = 0
+    for index in tqdm(range(len(metadata))):
+        pmc_path = metadata.pmc_json_files[index]
+        pdf_path = metadata.pdf_json_files[index]
+        if not pd.isna(pmc_path):
+            pmc_path = pj(path, pmc_path)
+            data = handle_pmc_json(metadata, index, pmc_path, path)
+        elif not pd.isna(pdf_path):
+            pdf_paths = [x.strip() for x in pdf_path.split(";")]
+            pdf_path = pj(path, pdf_paths[0])
+            data = handle_pdf_json(metadata, index, pdf_path)
+        else:
+            data = []
+
+        if len(data) and data[1]:
+            publish_time = metadata.publish_time[index]
+            journal = metadata.journal[index]
+
+            if pd.isna(publish_time):
+                publish_time = ""
+
+            if pd.isna(journal):
+                journal = ""
+
+            data.append(publish_time)
+            data.append(journal)
+            paper_df.writerow(data)
+
+            paper_count += 1
+
+    print("Total papers fetched:", paper_count)
 
 
 if __name__ == '__main__':
